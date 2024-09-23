121a933501e9d04904207794f5642500ee24ecf5
Danny Smart <danny.smart@lexisnexisrisk.com>
2024-09-23 16:53:45 +0100

    Add a ResponseFactory

---
+++ src/Response/ResponseFactory.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Lexide\LazyBoy\Response;
+
+use GuzzleHttp\Psr7\Response;
+use Psr\Http\Message\ResponseInterface;
+
+class ResponseFactory
+{
+    /**
+     * @param mixed $data
+     * @param int $statusCode
+     * @return ResponseInterface
+     */
+    public function createJson(mixed $data, int $statusCode = 200): ResponseInterface
+    {
+        return new Response(
+            $statusCode,
+            ["Content-Type" => "application/json"],
+            json_encode($data)
+        );
+    }
+
+    /**
+     * @return ResponseInterface
+     */
+    public function createNoContent(): ResponseInterface
+    {
+        return $this->createResponse(204);
+    }
+
+    /**
+     * @return ResponseInterface
+     */
+    public function createNotFound(): ResponseInterface
+    {
+        return $this->createError("Not found", 404);
+    }
+
+    /**
+     * @param string $message
+     * @param int $code
+     * @param ?array $context
+     * @return ResponseInterface
+     */
+    public function createError(string $message, int $code = 400, ?array $context = null): ResponseInterface
+    {
+        $payload = [
+            "error" => $message
+        ];
+        if (!is_null($context)) {
+            $payload["context"] = $context;
+        }
+
+        return $this->createJson($payload, $code);
+    }
+
+    /**
+     * @param int $statusCode
+     * @return ResponseInterface
+     */
+    public function createResponse(int $statusCode = 200): ResponseInterface
+    {
+        return new Response($statusCode);
+    }
+
+}
