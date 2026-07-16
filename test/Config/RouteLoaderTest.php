dafcb0d26f121bbe056e1fe335005215ec3a4b03
Danny Smart <danny.smart@lexisnexisrisk.com>
2026-07-16 10:04:25 +0100

    Migrate to use anonymous classes instead of implementing mock classes

--- test/Config/RouteLoaderTest.php
+++ test/Config/RouteLoaderTest.php
@@ -26,7 +26,7 @@ class RouteLoaderTest extends TestCase
     protected RouteGroupInterface|MockInterface $group;
     protected ServerRequestInterface|MockInterface $request;
     protected ResponseInterface|MockInterface $response;
-    protected MockController $controller;
+    protected object $controller;
 
     public function setUp(): void
     {
@@ -36,7 +36,77 @@ class RouteLoaderTest extends TestCase
         $this->group = \Mockery::mock(RouteGroupInterface::class);
         $this->request = \Mockery::mock(ServerRequestInterface::class);
         $this->response = \Mockery::mock(ResponseInterface::class);
-        $this->controller = new MockController();
+        $this->controller = $this->createController();
+    }
+
+    protected function createController(): object
+    {
+        return new class {
+
+            protected array $callCount = [];
+
+            public function callFoo(): void
+            {
+                $this->recordCall(__FUNCTION__);
+            }
+
+            public function callBar(): void
+            {
+                $this->recordCall(__FUNCTION__);
+            }
+
+            public function callBaz(): void
+            {
+                $this->recordCall(__FUNCTION__);
+            }
+
+            public function addRequest($request): void
+            {
+                $this->recordCall(__FUNCTION__);
+            }
+
+            public function addRequestInterface(RequestInterface $req): void
+            {
+                $this->recordCall(__FUNCTION__);
+            }
+
+            public function addResponse($response): void
+            {
+                $this->recordCall(__FUNCTION__);
+            }
+
+            public function addResponseInterface(ResponseInterface $resp): void
+            {
+                $this->recordCall(__FUNCTION__);
+            }
+
+            public function addArg(string $foo): void
+            {
+                $this->recordCall(__FUNCTION__);
+            }
+
+            public function addOptionalArg(string $foo = ""): void
+            {
+                $this->recordCall(__FUNCTION__);
+            }
+
+            public function addAll(RequestInterface $request, string $foo, int $bar, ResponseInterface $response): void
+            {
+                $this->recordCall(__FUNCTION__);
+            }
+
+            protected function recordCall(string $function): void
+            {
+                $this->callCount[$function] = $this->callCount[$function] ?? 0;
+                ++$this->callCount[$function];
+            }
+
+            public function getCallCount(): array
+            {
+                return $this->callCount;
+            }
+
+        };
     }
 
 
